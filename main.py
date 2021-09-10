1f3ab721261f3a73e79564653df6a4f699bec1f4
nesmachnow <sergion@fing.edu.uy>
2021-09-10 07:43:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def main(args):
     # Load and preprocess data for model
     #########################
 
-    data_path = "data/stock.csv"
+    data_path = "TimeGAN/data/stock.csv"
     X, T, _, args.max_seq_len, args.padding_value = data_preprocess(
         data_path, args.max_seq_len
     )
